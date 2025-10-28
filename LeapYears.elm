5d0581af03630e2a44e0da6c3a21b04db1cc0798
Sachin Sharma <59499244+sachin31301@users.noreply.github.com>
2025-10-28 15:12:06 +0530

    refactored code

--- LeapYears.elm
+++ LeapYears.elm
@@ -1,91 +1,122 @@
 module LeapYears exposing (..)
 import Browser exposing (..)
-import Html.Attributes exposing (style)
 import Html exposing (..)
-import Html.Attributes exposing (placeholder)
-import Html.Attributes exposing (value)
-import Html.Events exposing (onInput)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (style, placeholder, value)
+import Html.Events exposing (onInput, onClick)
 
 
-
+main : Program () Model Msg
 main =
-      Browser.element {
-        init=init,
-        view=view,
-        update=update,
-        subscriptions=subscriptions
-      }
+    Browser.element {
+        init = init,
+        view = view,
+        update = update,
+        subscriptions = subscriptions
+    }
 
-type alias Model = 
-                    {
-                        startYear: String,
-                        endYear  : String,
-                        error : Maybe String,
-                        leapYears : List Int
-                    }
 
-init : () ->   (Model, Cmd Msg) 
+type alias Model =
+    { startYear : String
+    , endYear : String
+    , error : Maybe String
+    , leapYears : List Int
+    }
+
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-      ({ startYear = "", endYear ="", error = Nothing , leapYears =[]}, Cmd.none)
+    ( { startYear = "", endYear = "", error = Nothing, leapYears = [] }, Cmd.none )
 
-type Msg = 
-            UpdateStartYear String |
-            UpdateEndYear String |
-            ShowLeapYears
+
+type Msg
+    = UpdateStartYear String
+    | UpdateEndYear String
+    | ShowLeapYears
+
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions _ =
     Sub.none
-update : Msg -> Model ->( Model ,Cmd Msg )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-         UpdateStartYear year ->
-           ( { model | startYear=year, error= Nothing, leapYears=[]} , Cmd.none)
-         UpdateEndYear year  ->
-           ( { model | endYear =year, error=Nothing, leapYears=[]} , Cmd.none)
-         ShowLeapYears ->
-            case ( String.toInt model.startYear , String.toInt model.endYear) of
-                (Just start, Just end)->
-                    if start > end then
-                       ( {model | error = Just " End Year must be Greater than start year"} , Cmd.none)
-                    else
-                        let
-                            yearsToCheck = List.range (start+1) (end - 1) 
-                            leapYears = List.filter checkLeapYear yearsToCheck
-                        in
-                            ({model | leapYears =leapYears , error = Nothing, startYear= "", endYear=""} , Cmd.none)
-                        
+        UpdateStartYear year ->
+            ( { model | startYear = year, error = Nothing, leapYears = [] }, Cmd.none )
 
-                _ ->
-                    ({model | error = Just " Please enter valid years"} , Cmd.none)
+        UpdateEndYear year ->
+            ( { model | endYear = year, error = Nothing, leapYears = [] }, Cmd.none )
 
+        ShowLeapYears ->
+            let
+                ( maybeError, leapList ) =
+                    validateAndFindLeapYears model.startYear model.endYear
+            in
+            ( { model
+                | error = maybeError
+                , leapYears = leapList
+                , startYear = ""
+                , endYear = ""
+              }
+            , Cmd.none
+            )
+
+
+validateAndFindLeapYears : String -> String -> ( Maybe String, List Int )
+validateAndFindLeapYears startStr endStr =
+    case ( String.toInt startStr, String.toInt endStr ) of
+        ( Just start, Just end ) ->
+            if start > end then
+                ( Just "End Year must be greater than Start Year", [] )
+
+            else
+                let
+                    yearsToCheck =
+                        List.range (start + 1) (end - 1)
+
+                    leapYears =
+                        List.filter checkLeapYear yearsToCheck
+                in
+                ( Nothing, leapYears )
+
+        _ ->
+            ( Just "Please enter valid years", [] )
+
+
+checkLeapYear : Int -> Bool
 checkLeapYear year =
-            (modBy 4 year == 0 && modBy 100 year /= 0)
+    (modBy 4 year == 0 && modBy 100 year /= 0)
         || (modBy 400 year == 0)
 
 
 view : Model -> Html Msg
 view model =
-    div [ style "text-align" "center", style "margin" "40px" ] [
-        h1 [][ text " Please enter start year and end year"],
-        input [ placeholder "Start Year" , value model.startYear , onInput UpdateStartYear] [],
-        input [ placeholder  "End Year" ,value model.endYear, onInput UpdateEndYear] [],
-        button [ onClick ShowLeapYears][text "Show Leap Years"] ,
+    div [ style "text-align" "center", style "margin" "40px" ]
+        [ h1 [] [ text "Please enter start year and end year" ]
+        , input [ placeholder "Start Year", value model.startYear, onInput UpdateStartYear ] []
+        , input [ placeholder "End Year", value model.endYear, onInput UpdateEndYear ] []
+        , button [ onClick ShowLeapYears ] [ text "Show Leap Years" ]
+        , viewErrorOrLeapYears model
+        ]
 
-        case model.error of 
-            Just msg ->
-                div [ style "color" "red" , style "margin-top" "25px"][text msg]
-            Nothing ->
-                if List.isEmpty model.leapYears then
-                    text ""
-                else 
-                    div [ style "margin-top" "30px"] [ 
-                        h1 [][text "List of Leap Years"],
-                        ul [] ( List.map viewYear model.leapYears)
+
+viewErrorOrLeapYears : Model -> Html Msg
+viewErrorOrLeapYears model =
+    case model.error of
+        Just msg ->
+            div [ style "color" "red", style "margin-top" "25px" ] [ text msg ]
+
+        Nothing ->
+            if List.isEmpty model.leapYears then
+                text ""
+            else
+                div [ style "margin-top" "30px" ]
+                    [ h1 [] [ text "List of Leap Years" ]
+                    , ul [] (List.map viewYear model.leapYears)
                     ]
-    ]
 
+
+viewYear : Int -> Html msg
 viewYear year =
-                li [] [text (String.fromInt year)]
-    
+    li [] [ text (String.fromInt year) ]
